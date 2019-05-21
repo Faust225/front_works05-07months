7956e21941491b6bffd241be0d94ee6c75a2df31
Oskar <oskaras2011@yandex.com>
2019-05-21 10:48:29 +0300

    yesterday basics done

---
+++ today-basics/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<!-- Font awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+	integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+	crossorigin="anonymous">
+
+	<meta charset="utf-8">
+	<title>Basics</title>
+</head>
+<body>
+<header>
+	<div class="header-elements">
+		<img src="css/images/google.gif">
+		<nav>
+			<a href="">Home</a>
+			<a href="">About</a>
+			<a href="">Contacts</a>
+		</nav>
+	</div>
+</header>
+<ul>
+	<li>Home</li>
+	<li>About</li>
+	<li>Contacts</li>
+</ul>
+
+<ol>
+	<li><a href="https://www.google.com/">Home</a></li>
+	<li><a href="https://www.google.com/">About</a></li>
+	<li><a href="https://www.google.com/">Contacts</a></li>
+</ol>
+<div class="task-3">
+	<p>This paragraph is red</p>
+	<p>This paragraph is blue</p>
+	<p>This paragraph is green</p>
+</div>
+<div class="task3-2">
+	<p>This paragraph is red</p>
+	<p>This paragraph is blue</p>
+	<p>This paragraph is green</p>
+</div>
+<div class="container">
+	<article>
+		<h2>Title</h2>
+		<p>UGuu ihef ihegf ihei piodj</p>
+	</article>
+	<article>
+		<h2>Title</h2>
+		<p>UGuu ihef ihegf ihei piodj</p>
+	</article>
+	<article>
+		<h2>Title</h2>
+		<p>UGuu ihef ihegf ihei piodj</p>
+	</article>
+	<article>
+		<h2>Title</h2>
+		<p>UGuu ihef ihegf ihei piodj</p>
+	</article>
+	<article>
+		<h2>Title</h2>
+		<p>UGuu ihef ihegf ihei piodj</p>
+	</article>
+	<article>
+		<h2>Title</h2>
+		<p>UGuu ihef ihegf ihei piodj</p>
+	</article>
+	<article>
+		<h2>Title</h2>
+		<p>UGuu ihef ihegf ihei piodj</p>
+	</article>
+	<article>
+		<h2>Title</h2>
+		<p>UGuu ihef ihegf ihei piodj</p>
+	</article>
+</div>
+<div class="eagle-header">
+	<div class="eagle-nav-pos">
+		<a href="">Me</a>
+		<nav>
+			<a href="">WHO</a>
+			<a href="">WHAT</a>
+			<a href="">WHERE</a>
+		</nav>
+	</div>
+</div>
+<div class="task-6">
+	<div class="eagle">
+		<h2>Who Am I?</h2>
+			<img src="css/images/eagle.jpg">
+		<h2>I'm an adventurer</h2>
+	</div>
+</div>
+	<footer>
+		<h2>What I am I?</h2>
+		<p> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<a href=""><i class="fas fa-search"></i>Search</a>
+	</footer>
+</body>
+</html>
